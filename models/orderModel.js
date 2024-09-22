adccc41f5cdc015767d0bf8d3e5cc1617423524e
hanaessam <hanaessamsayed@gmail.com>
2024-09-22 22:19:36 +0300

    Order

---
+++ models/orderModel.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User is required"],
+    },
+    items: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: Number,
+        model: String,
+        price: Number,
+      },
+    ],
+    tax: {
+      type: Number,
+      default: 0,
+    },
+    shipping: {
+      type: Number,
+      default: 0,
+    },
+    totalPrice: Number,
+    payment: {
+      type: String,
+      enum: ["cash", "card"],
+      default: "cash",
+    },
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+    paidAt: Date,
+    isDelivered: {
+      type: Boolean,
+      default: false,
+    },
+    deliveredAt: Date,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
